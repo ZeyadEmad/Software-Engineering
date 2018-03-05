a2ada1b85f8808c6401bac59a2d75a30587e2534
ZeyadEmad <32908599+ZeyadEmad@users.noreply.github.com>
2018-03-05 14:48:26 +0200

    ...

---
+++ Worker/php/Teacher.php
@@ -0,0 +1,71 @@
+<?php
+
+?>
+
+<html>
+    
+    <head>
+        <title>
+            Teachers
+        </title>
+    </head>
+
+    <body>
+        <table>
+            <thead>
+                <tr>
+                  <th>
+                      Saturday
+                  </th>
+                  <th>
+                      Sunday
+                  </th>
+                  <th>
+                      Monday
+                  </th>
+                  <th>
+                      Tuesday
+                  </th>
+                  <th>
+                      Wednesday
+                  </th>
+                  <th>
+                      Thursday
+                  </th>
+                  <th>
+                      Friday
+                  </th>
+                </tr>
+                
+            </thead>
+
+                <tr>
+                    <th>
+                          Dummy Data
+                    </th>
+                    <th>
+                          Dummy Data
+                    </th>
+                    <th>
+                          Dummy Data
+                    </th>
+                    <th>
+                          Dummy Data
+                    </th>
+                    <th>
+                          Dummy Data
+                    </th>
+                    <th>
+                          Dummy Data
+                    </th>
+                    <th>
+                          Dummy Data
+                    </th>
+
+                </tr>
+        </table>
+    </body>
+    
+</html>
+
+
