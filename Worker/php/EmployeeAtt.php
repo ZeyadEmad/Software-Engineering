a36af81cc291efd3d737c0bd86f90a6b95ce5d98
Alaa15 <alaabaghdady22@gmail.com>
2018-03-06 21:46:20 +0200

    html user

--- Worker/php/EmployeeAtt.php
+++ Worker/php/EmployeeAtt.php
@@ -1,12 +1,90 @@
 <?php
 ?>
 <html>
-    <title>
-        Employee Attendance
-    </title>
-
-    <body>
+    <head>
+        <title>
+            Employee Attendance
+        </title>
+        <link rel="stylesheet" type="text/css" href="../css/EmployeeAtt.css">
         
+    </head>
+    <body>
+        <br>
+        <br>
+        <a href="HomePage.php">Go back</a>
+        <br>
+        <br>
+        
+        
+        <table>
+              <thead>
+                <tr>
+                    <th>
+                        Employee Name
+                    </th>
+                    <th>
+                        Saturday
+                    </th>
+                    <th>
+                        Sunday
+                    </th>
+                    <th>
+                        Monday
+                    </th>
+                    <th>
+                        Tuesday
+                    </th>
+                    <th>
+                        Wednesday
+                    </th>
+                    <th>
+                        Thursday
+                    </th>
+                    <th>
+                        Friday
+                    </th>
+              </tr>
+                  
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>
+                        Dummy
+                      </td>
+                      <td>
+                        Dummy
+                          <input type="checkbox" name="day1">
+                      </td>
+                      <td>
+                        Dummy
+                          <input type="checkbox" name="day2">
+                      </td>
+                      <td>
+                        Dummy
+                          <input type="checkbox" name="day3">
+                      </td>
+                      <td>
+                        Dummy
+                          <input type="checkbox" name="day4">
+                      </td>
+                      <td>
+                        Dummy
+                          <input type="checkbox" name="day5">
+                      </td>
+                      <td>
+                        Dummy
+                          <input type="checkbox" name="day6">
+                      </td>
+                      <td>
+                        Dummy
+                          <input type="checkbox" name="day7">
+                      </td>
+                  </tr>
+                  
+              </tbody>
+
+            
+        </table>
     </body>
 </html>
 
